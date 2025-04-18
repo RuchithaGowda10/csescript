f5963aadf53c367f40aee3e66f2db09ba0a1f964
RuchithaGowda10 <ruchithagowda2510@gmail.com>
2025-04-18 12:06:27 +0530

    Update RGVMCSEDEP.ps1

--- RGVMCSEDEP.ps1
+++ RGVMCSEDEP.ps1
@@ -4,12 +4,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Install Visual Studio Code
-choco install vscode -y
-
-# Install Python
-choco install python -y
-
-# Install Git (Git Bash included)
-choco install git -y
