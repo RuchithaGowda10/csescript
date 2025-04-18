cef25c6b9507aaeeeb17323d89300201e7ad1036
RuchithaGowda10 <ruchithagowda2510@gmail.com>
2025-04-18 12:34:51 +0530

    Update RGVMCSEDEP.ps1

--- RGVMCSEDEP.ps1
+++ RGVMCSEDEP.ps1
@@ -1,6 +1,12 @@
-# install-devtools.ps1
+Start-Transcript -Path "C:\cse_log.txt" -Append
 
-# Install Chocolatey (Windows package manager)
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Start-Process cmd.exe -ArgumentList "/c iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" -Wait
+
+choco install vscode -y
+choco install python -y
+choco install git -y
+
+Stop-Transcript
